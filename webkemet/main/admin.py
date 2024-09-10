cf59d93424296fb344471925ce32dbfa3b2d71ff
JH <boonzaaierjh444@gmail.com>
2024-09-10 22:03:27 +0200

    Delete webkemet directory

--- webkemet/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Figure
-
-
-@admin.register(Figure)
-class FigureAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    search_fields = ('name',)  
-
